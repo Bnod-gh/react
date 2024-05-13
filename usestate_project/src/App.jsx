8afa5d6dfabee12aceafa1e65eb4c397fea480e0
bnod-gh <ghbinod1991@gmail.com>
2024-05-13 19:12:46 +0930

    h

--- usestate_project/src/App.jsx
+++ usestate_project/src/App.jsx
@@ -4,11 +4,15 @@ import image_icon from './assets/image/bino.jpg'
 
 function App() {
   const [count, setCount] = useState(true);
+  const [bgdiv,setBgdiv]= useState(true)
 
   const showbtn =count?'Hide':'Show';
   
+  const click =bgdiv?"Red":"Blue"
 
 
+  
+  
   return (
     <div className='min-h-screen bg-slate-500'>
       <div className='w-3/6 bg-white h-40 m-auto'>
@@ -22,12 +26,19 @@ function App() {
         <img src ={image_icon} className= 'h-full cover w-full ' alt=""  />
       </div>:<div className='m-auto w-3/6 bg-white h-96 mb-2'></div>}
       
-      <button className='flex flex-col justify-center items-center bg-white p-2 m-auto rounded-xl w-14' onClick={()=>{setCount(!count)}}>
+      <button className='flex flex-col justify-center items-center bg-blue-600 text-white text-bold p-2 m-auto rounded-xl w-14' onClick={()=>{setCount(!count)}}>
     {showbtn}
     
   </button>
 
+<div className='flex justify-center items-center flex-col'>
+
+<div className='w-96 h-96 pt-5 m-3 ' style={{backgroundColor:(bgdiv?"Blue":"Red")}}> </div>
+   
+  <button className='w-auto p-2 m-auto text-bold text-white bg-black rounded-xl' onClick={()=>{setBgdiv(!bgdiv)}} >{click}</button>
     </div>
+    </div>  
+
   )
 }
 
